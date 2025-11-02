0aa8f3d18db9fafc4bd8cf9e3e934837f6482a46
tuzhu <tuzhu003@outlook.com>
2025-11-02 18:46:14 +0800

    完成了后台管理_用户相关的全部功能

--- web/web-admin/src/main/java/com/lease/web/admin/controller/user/UserInfoController.java
+++ web/web-admin/src/main/java/com/lease/web/admin/controller/user/UserInfoController.java
@@ -1,29 +1,46 @@
 package com.lease.web.admin.controller.user;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.lease.common.result.Result;
 import com.lease.model.entity.UserInfo;
 import com.lease.model.enums.BaseStatus;
+import com.lease.web.admin.service.UserInfoService;
 import com.lease.web.admin.vo.user.UserInfoQueryVo;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "用户信息管理")
 @RestController
 @RequestMapping("/admin/user")
+@RequiredArgsConstructor
 public class UserInfoController {
+
+    private final UserInfoService userInfoService;
 
     @Operation(summary = "分页查询用户信息")
     @GetMapping("page")
     public Result<IPage<UserInfo>> pageUserInfo(@RequestParam long current, @RequestParam long size, UserInfoQueryVo queryVo) {
-        return Result.ok();
+        IPage<UserInfo> page = new Page<>(current, size);
+        LambdaQueryWrapper<UserInfo> queryWrapper = new LambdaQueryWrapper<UserInfo>()
+                .like(queryVo.getPhone() != null, UserInfo::getPhone, queryVo.getPhone())
+                .eq(queryVo.getStatus() != null, UserInfo::getStatus, queryVo.getStatus());
+        IPage<UserInfo> pageResult = userInfoService.page(page, queryWrapper);
+        return Result.ok(pageResult);
     }
 
     @Operation(summary = "根据用户id更新账号状态")
     @PostMapping("updateStatusById")
     public Result updateStatusById(@RequestParam Long id, @RequestParam BaseStatus status) {
+        LambdaUpdateWrapper<UserInfo> updateWrapper = new LambdaUpdateWrapper<UserInfo>()
+                .eq(UserInfo::getId, id)
+                .set(UserInfo::getStatus, status);
+        userInfoService.update(updateWrapper);
         return Result.ok();
     }
 }
