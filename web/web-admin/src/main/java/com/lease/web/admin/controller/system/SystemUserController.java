d7402a0e844ef97f335bafd6d0bb104594b26437
tuzhu <tuzhu003@outlook.com>
2025-11-04 23:33:26 +0800

    完成了后台用户的所有接口

--- web/web-admin/src/main/java/com/lease/web/admin/controller/system/SystemUserController.java
+++ web/web-admin/src/main/java/com/lease/web/admin/controller/system/SystemUserController.java
@@ -1,6 +1,8 @@
 package com.lease.web.admin.controller.system;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.lease.common.result.Result;
@@ -12,6 +14,7 @@ import com.lease.web.admin.vo.system.user.SystemUserQueryVo;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -41,24 +44,36 @@ public class SystemUserController {
     @Operation(summary = "保存或更新后台用户信息")
     @PostMapping("saveOrUpdate")
     public Result saveOrUpdate(@RequestBody SystemUser systemUser) {
+        if(systemUser.getPassword() != null) {
+            systemUser.setPassword(DigestUtils.md5Hex(systemUser.getPassword()));
+        }
+        systemUserService.saveOrUpdate(systemUser);
         return Result.ok();
     }
 
     @Operation(summary = "判断后台用户名是否可用")
     @GetMapping("isUserNameAvailable")
     public Result<Boolean> isUsernameExists(@RequestParam String username) {
-        return Result.ok();
+        LambdaQueryWrapper<SystemUser> queryWrapper = new LambdaQueryWrapper<SystemUser>()
+                .eq(SystemUser::getUsername, username);
+        long count = systemUserService.count(queryWrapper);
+        return Result.ok(count == 0);
     }
 
     @DeleteMapping("deleteById")
     @Operation(summary = "根据ID删除后台用户信息")
     public Result removeById(@RequestParam Long id) {
+        systemUserService.removeById(id);
         return Result.ok();
     }
 
     @Operation(summary = "根据ID修改后台用户状态")
     @PostMapping("updateStatusByUserId")
     public Result updateStatusByUserId(@RequestParam Long id, @RequestParam BaseStatus status) {
+        LambdaUpdateWrapper<SystemUser> updateWrapper = new LambdaUpdateWrapper<>();
+        updateWrapper.eq(SystemUser::getId, id);
+        updateWrapper.set(SystemUser::getStatus, status);
+        systemUserService.update(updateWrapper);
         return Result.ok();
     }
 }
